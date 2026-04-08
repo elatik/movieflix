8bf327f57873312aa05887fe2d8ddef06a6b7edb
elatik <elatik@yahoo.com>
2026-04-08 03:13:08 +0100

    Display Popular Movies

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-// API key
-const API_KEY = "c2c40e0f8827f0ad88e3922f3cc17ce6";
-
 // Theme toggle
 const html = document.documentElement;
 const savedTheme = localStorage.getItem("theme");
@@ -13,10 +10,48 @@ document.getElementById("themeToggle").addEventListener("click", () => {
   localStorage.setItem("theme", newTheme);
 });
 
-
+// Global variables
 const global = {
   currentPage: window.location.pathname,
 };
+
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData("movie/popular");
+  results.forEach((movie) => {
+    const div = document.createElement("div");
+    div.classList.add("movie-card");
+    const posterImage = movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : "/images/NoImage.png";
+    div.innerHTML = `
+          <div class="card-poster">
+            <img src="${posterImage}" alt="${movie.title}" loading="lazy" />
+            <div class="card-overlay">
+              <a href="/movie-details.html?id=${movie.id}" class="play-btn">
+                <svg viewBox="0 0 24 24">
+                  <polygon points="5 3 19 12 5 21 5 3" />
+                </svg>
+              </a>
+            </div>
+          </div>
+          <div class="card-info">
+            <div class="card-title">${movie.title}</div>
+            <div class="card-date">${movie.release_date}</div>
+          </div>
+    `;
+    document.getElementById("movieGrid").appendChild(div);
+  });
+}
+
+// Fetch data from TMDB API
+async function fetchAPIData(endpoint) {
+  // API key
+  const API_KEY = "c2c40e0f8827f0ad88e3922f3cc17ce6";
+  const API_URL = `https://api.themoviedb.org/3/`;
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`,
+  );
+  const data = await response.json();
+  return data;
+}
 
 // Highlight active nav link
 function highlightActiveLink() {
@@ -33,7 +68,7 @@ function init() {
   switch (global.currentPage) {
     case "/":
     case "/index.html":
-      console.log("Home");
+      displayPopularMovies();
       break;
     case "/shows.html":
       console.log("TV Shows");
