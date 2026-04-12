f87185658dcb6ff945baa1a0e0eb4248f9b81235
elatik <elatik@yahoo.com>
2026-04-12 11:00:51 +0100

    Display details pages now working proparly

--- js/script.js
+++ js/script.js
@@ -71,6 +71,65 @@ async function displayPopularTVShows() {
   });
 }
 
+// Display Movie Details
+async function displayMovieDetails() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const movieId = urlParams.get("id");
+  const movie = await fetchAPIData(`movie/${movieId}`);
+  document.getElementById("movieTitle").textContent = movie.title;
+  document.getElementById("movieGenre").textContent = movie.genres.map((genre) => genre.name).join(", ");
+  document.getElementById("movieRating").textContent = movie.vote_average.toFixed(1);
+  document.getElementById("movieOverview").textContent = movie.overview;
+  const posterImage = movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : "/images/NoImage.png";
+  document.getElementById("moviePoster").src = posterImage;
+  
+  // Populate actors list
+  const credits = await fetchAPIData(`movie/${movieId}/credits`);
+  const actorsList = document.getElementById("actorsList");
+  actorsList.innerHTML = credits.cast.slice(0, 5).map(actor => `<li>${actor.name}</li>`).join("");
+  document.getElementById("actorsMoreLink").addEventListener("click", (e) => {
+    if (e.target.textContent === "More") {
+      actorsList.innerHTML = credits.cast.map(actor => `<li>${actor.name}</li>`).join("");
+      e.target.textContent = "Less";
+    } else {
+      actorsList.innerHTML = credits.cast.slice(0, 7).map(actor => `<li>${actor.name}</li>`).join("");
+      e.target.textContent = "More";
+    }
+  });
+  // TODO: Add more details like director, runtime, etc.
+  document.getElementById("directorList").innerHTML = credits.crew.filter((crew) => crew.job === "Director").map((crew) => `<li>${crew.name}</li>`).join("");
+  document.getElementById("screenwriterList").innerHTML = credits.crew.filter((crew) => crew.job === "Screenplay").map((crew) => `<li>${crew.name}</li>`).join("");
+  document.getElementById("producersList").innerHTML = credits.crew.filter((crew) => crew.job === "Producer").map((crew) => `<li>${crew.name}</li>`).join("");
+}
+
+// Display TV Show Details
+async function displayTvDetails() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const tvId = urlParams.get("id");
+  const show = await fetchAPIData(`tv/${tvId}`);
+  document.getElementById("show-title").textContent = show.name;
+  document.getElementById("show-genre").textContent = show.genres.map((genre) => genre.name).join(", ");
+  document.getElementById("show-rating").textContent = show.vote_average.toFixed(1);
+  document.getElementById("show-overview").textContent = show.overview;
+  const posterImage = show.poster_path ? `https://image.tmdb.org/t/p/w500${show.poster_path}` : "/images/NoImage.png";
+  document.getElementById("show-poster").src = posterImage;
+  
+  // Populate actors list
+  const credits = await fetchAPIData(`tv/${tvId}/credits`);
+  console.log(credits);
+  const actorsList = document.getElementById("actorsList");
+  actorsList.innerHTML = credits.cast.slice(0, 5).map(actor => `<li>${actor.name}</li>`).join("");
+  document.getElementById("actorsMoreLink").addEventListener("click", (e) => {
+    if (e.target.textContent === "More") {
+      actorsList.innerHTML = credits.cast.map(actor => `<li>${actor.name}</li>`).join("");
+      e.target.textContent = "Less";
+    } else {
+      actorsList.innerHTML = credits.cast.slice(0, 7).map(actor => `<li>${actor.name}</li>`).join("");
+      e.target.textContent = "More";
+    }
+  });
+}
+
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
   // API key
@@ -115,10 +174,10 @@ function init() {
       displayPopularTVShows();
       break;
     case "/movie-details.html":
-      console.log("Movie Details");
+      displayMovieDetails();
       break;
     case "/tv-details.html":
-      console.log("TV Show Details");
+      displayTvDetails();
       break;
     case "/search.html":
       console.log("Search");
